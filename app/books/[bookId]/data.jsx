320d4b35ea41a2c9547619479548764a84c79e68
Taku <shinimini545@gmail.com>
2026-09-25 21:57:04 +0800

    Add data file for dementia-related books
    This file contains a list of books related to dementia, each with modules covering various stages and topics.

---
+++ app/books/[bookId]/data.jsx
@@ -0,0 +1,72 @@
+//Added File
+//Unfinished until I finish researching paper for content
+export const books = [
+  {
+    id: '1',
+    title: 'Dementia and Its Stages',
+    modules: [
+      { id: '1', title: "What is Dementia?" },
+      { id: '2', title: "Early Stage" },
+      { id: '3', title: "Middle Stage" },
+      { id: '4', title: "Late Stage" }
+    ]
+  },
+
+  {
+    id: '2',
+    title: 'Navigating Day-to-Day Interactions and Behavior Changes',
+     modules: [
+      { id: '1', title: "Dementia" },
+      { id: '2', title: "Early Stage" },
+      { id: '3', title: "Middle Stage" },
+      { id: '4', title: "Late Stage" }
+    ]
+  },
+
+  {
+    id: '3',
+    title: 'Daily Caregiving Skills for Early Stage',
+     modules: [
+      { id: '1', title: "What is Dementia?" },
+      { id: '2', title: "Early Stage" },
+      { id: '3', title: "Middle Stage" },
+      { id: '4', title: "Late Stage" }
+    ]
+  },
+
+  {
+    id: '4',
+    title: 'Daily Caregiving Skills for Middle to Late Stage',
+    modules: [
+      { id: '1', title: "What is Dementia?" },
+      { id: '2', title: "Early Stage" },
+      { id: '3', title: "Middle Stage" },
+      { id: '4', title: "Late Stage" }
+    ]
+  },
+
+  {
+    id: '5',
+    title: 'Signs of an Incoming Episode or Emergency',
+     modules: [
+      { id: '1', title: "What is Dementia?" },
+      { id: '2', title: "Early Stage" },
+      { id: '3', title: "Middle Stage" },
+      { id: '4', title: "Late Stage" }
+    ]
+  },
+
+  {
+    id: '6',
+    title: 'Emergency Preparation and Drills',
+     modules: [
+      { id: '1', title: "What is Dementia?" },
+      { id: '2', title: "Early Stage" },
+      { id: '3', title: "Middle Stage" },
+      { id: '4', title: "Late Stage" }
+    ]
+  },
+];
+
+
+
