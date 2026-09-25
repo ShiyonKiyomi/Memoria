2a49613a7024c5df012da4b5a24da7cd479ba9f8
Taku <shinimini545@gmail.com>
2026-09-25 21:47:12 +0800

    Refactor BookList component with SafeAreaView

--- app/books/index.jsx
+++ app/books/index.jsx
@@ -1,21 +1,60 @@
-// app/books/index.jsx
-// STUB — BOOKS: top-level list of care books/topics (was courses/index.jsx)
-// Replace this with: import BookList from "../src/screens/.../BookList";
-//                     export default BookList;
+//Adjusted File
 
 import { View, Text, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { useWindowDimensions } from "react-native";
+import { colors } from "../../src/theme";
+import BackButton from "../../src/components/back-button"
+import { BookEntry } from "../../src/components/module-component";
 
-export default function BookListStub() {
+export default function BookList() {
+  const {height, width,} = useWindowDimensions();
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Books</Text>
-      <Link href="/books/book123">Go to Book "book123"</Link>
-    </View>
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+
+        <View style={[styles.header, { width: width, height: height*0.085 }]}>
+          <BackButton />
+          <Text style={styles.header_text}>Books</Text>
+        </View>
+
+        <View style={styles.button_container}>
+          <BookEntry path={ '/books/1' } book={'Book 1'} title={'Dementia and Its Stages'}/>
+          <BookEntry path={ '/books/2' } book={'Book 2'} title={'Navigating Day-to-Day Interactions and Behavior Changes'}/>
+          <BookEntry path={ '/books/3' } book={'Book 3'} title={'Daily Caregiving Skills for the Early Stage'}/>
+          <BookEntry path={ '/books/4' } book={'Book 4'} title={'Daily Caregiving Skills for the Middle to Late Stage'}/>
+          <BookEntry path={ '/books/5' } book={'Book 5'} title={'Signs of an Incoming Episode or Emergency'}/>
+          <BookEntry path={ '/books/6' } book={'Book 6'} title={'Emergency Preparation and Drills'}/>
+        </View>
+
+    </SafeAreaView>
+  </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", justifyContent: "center", gap: 12 },
-  text: { fontSize: 18, fontWeight: "600" },
+  container: {
+    flex: 1,
+  },
+
+  header: {
+    backgroundColor: colors.primaryTint15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 12,
+  },
+
+  header_text: {
+    fontSize: 22,
+    fontWeight: "600",
+    marginLeft: 10,
+    color: colors.textBody,
+  },
+
+  button_container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: colors.bgScreen,
+  },
 });
